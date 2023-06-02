2e1ddf5281c1ef1acd4a77cd10e01b05a9d8ad00
TurkeuBacon <99289161+TurkeuBacon@users.noreply.github.com>
2023-06-02 07:01:17 -0700

    SplashScreen done The splash screen image scales up slightly for 4 seconds. At the 3.5 second mark the screen fades to black over .5 seconds The Scene then transitions to the Intro

--- src/game.js
+++ src/game.js
@@ -1,4 +1,4 @@
-class SplashScreen extends Phaser.scene
+class SplashScreen extends Phaser.Scene
 {
     constructor()
     {
@@ -7,20 +7,48 @@ class SplashScreen extends Phaser.scene
 
     preload()
     {
+        this.load.path = '../assets/';
+        this.load.image('studioArt', 'TurkeuBaconGames.png');
     }
 
     create()
     {
+        this.displayTime = 4000;
+        this.fadeTime = 1000;
+        this.artSizes = {'start': 0.7, 'end': 0.8};
+        this.cam = this.cameras.main.setBackgroundColor(0x4a58a6);
+        this.canvasWidth = this.sys.game.canvas.width;
+        this.canvasHeight = this.sys.game.canvas.height;
+        let imageSize = this.artSizes.start * (this.canvasHeight < this.canvasWidth ? this.canvasHeight : this.canvasWidth);
+        this.studioArt = this.add.image(this.canvasWidth/2, this.canvasHeight/2, 'studioArt');
+        this.studioArt.setDisplaySize(imageSize, imageSize);
 
+        this.time.delayedCall(this.displayTime, () => {
+            this.scene.start('intro');
+        });
+
+        this.fadeRectangle = this.add.rectangle(0, 0, this.canvasWidth, this.canvasHeight, 0x000000, 1).setOrigin(0, 0).setAlpha(0);
+
+        this.sceneStartTime = this.game.getTime();
     }
 
     update()
     {
+        let currentTime = (this.game.getTime() - this.sceneStartTime);
+        let artScale = this.artSizes.start + (this.artSizes.end - this.artSizes.start) * (currentTime / this.displayTime);
+        let imageSize = artScale * (this.canvasHeight < this.canvasWidth ? this.canvasHeight : this.canvasWidth);
+        this.studioArt.setDisplaySize(imageSize, imageSize);
 
+        if(currentTime > this.displayTime - this.fadeTime)
+        {
+            let percent = 1 - (this.displayTime - currentTime) / this.fadeTime;
+            console.log(percent);
+            this.fadeRectangle.setAlpha(percent);
+        }
     }
 }
 
-class Intro extends Phaser.scene
+class Intro extends Phaser.Scene
 {
     constructor()
     {
@@ -43,7 +71,7 @@ class Intro extends Phaser.scene
     }
 }
 
-class MainMenu extends Phaser.scene
+class MainMenu extends Phaser.Scene
 {
     constructor()
     {
